6b821293bd895a0584a06fb7e8d6fc684f12e305
Cindy <crubin@acquirgy.com>
2014-11-04 16:37:39 -0500

    modify image for email-brochure.php

--- email-brochure.php
+++ email-brochure.php
@@ -362,7 +362,7 @@ style="display:none;width:0px;height:0px"></iframe>
                   <div class="slide">
 
                     <div id="slide_content">
-                      <img src="../sk/images/hot-tub.jpg" alt="ThermoSpas Hot Tub" width="570" height="584"/>
+                      <img src="../sk/images/hot-tub.jpg" alt="ThermoSpas Hot Tub" width="570" height="538"/>
                     </div>
                     <div id="subscribe_pricing" >
 
