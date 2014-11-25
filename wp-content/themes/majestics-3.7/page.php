3aa1423959d1b859aff8e2563b4aa1b2945a634c
Cindy <crubin@acquirgy.com>
2014-11-25 09:21:37 -0500

    modify validation on free-brochure-form

--- wp-content/themes/majestics-3.7/page.php
+++ wp-content/themes/majestics-3.7/page.php
@@ -74,7 +74,7 @@
                   </tr>
                   <tr>
                     <td colspan="2" align="center">
-                      <input name="email" id="email" type="text" class="textf" value="Your Email" />
+                      <input name="email" type="text" class="textf email" value="Your Email" />
                     </td>
                   </tr>
                   <tr>
@@ -191,12 +191,6 @@
           });
 
           $('.free-brochure-form').validate({
-            rules: {
-              email: {
-                required: true,
-                email: true
-              }
-            },
             focusInvalid: false,
             errorPlacement: function(error,element) {}
           });
@@ -206,6 +200,11 @@
         jQuery.validator.addMethod("defaultInvalid", function(value, element) {
             return !(element.value == element.defaultValue);
         },"This field is required.");
+
+        jQuery.validator.addMethod('email', function (value) {
+            return /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)*(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i.test(value);
+        }, 'Enter a valid email address');
+
       </script>
 
     <?php } ?>
